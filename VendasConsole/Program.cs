744dd7fb3f3146ddce8c7ed7b8880a05ba614cb0
Diogo Deconto <dideconto@gmail.com>
2020-08-10 21:17:00 -0300

    Adicionar menu na aplicação

--- VendasConsole/Program.cs
+++ VendasConsole/Program.cs
@@ -6,7 +6,35 @@ namespace VendasConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int opcao;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(" ---- APLICAÇÃO DE VENDAS ---- \n");
+                Console.WriteLine("1 - Cadastrar cliente");
+                Console.WriteLine("2 - Listar clientes");
+                Console.WriteLine("0 - Sair"); 
+                Console.WriteLine("\nDigite a opção desejada:");
+                opcao = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine(" ---- CADASTRAR CLIENTE ---- \n");
+                        break;
+                    case 2:
+                        Console.WriteLine(" ---- LISTAR CLIENTES ---- \n");
+                        break;
+                    case 0:
+                        Console.WriteLine("Saindo...\n");
+                        break;
+                    default:
+                        Console.WriteLine(" ---- OPÇÃO INVÁLIDA!!! ---- \n");
+                        break;
+                }
+                Console.WriteLine("\nPressione uma tecla para continuar...");
+                Console.ReadKey();
+            } while (opcao != 0);
         }
     }
 }
