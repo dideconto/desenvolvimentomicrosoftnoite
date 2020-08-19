79288c5d9a9b4ec8b753340232e1dc927bc59e6c
Diogo Deconto <dideconto@gmail.com>
2020-08-19 20:51:56 -0300

    Refatoração

---
+++ VendasConsole/DAL/ClienteDAO.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using VendasConsole.Models;
+
+namespace VendasConsole.DAL
+{
+    class ClienteDAO
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+        public static List<Cliente> Listar() => clientes;
+        public static bool Cadastrar(Cliente c)
+        {
+            if (BuscarCliente(c.Cpf) == null)
+            {
+                clientes.Add(c);
+                return true;
+            }
+            return false;
+        }
+        public static Cliente BuscarCliente(string cpf)
+        {
+            foreach (Cliente clienteCadastrado in clientes)
+            {
+                if (clienteCadastrado.Cpf == cpf)
+                {
+                    return clienteCadastrado;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ VendasConsole/Views/CadastrarCliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using VendasConsole.DAL;
+using VendasConsole.Models;
+using VendasConsole.Utils;
+
+namespace VendasConsole.Views
+{
+    class CadastrarCliente
+    {
+        public static void Renderizar()
+        {
+            Cliente c = new Cliente();
+            Console.WriteLine(" ---- CADASTRAR CLIENTE ---- \n");
+            Console.WriteLine("Digite o nome do cliente:");
+            c.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o cpf do cliente:");
+            c.Cpf = Console.ReadLine();
+            if (Validacao.ValidarCpf(c.Cpf))
+            {
+                if (ClienteDAO.Cadastrar(c))
+                {
+                    Console.WriteLine("Cliente cadastrado com sucesso!!!");
+                }
+                else
+                {
+                    Console.WriteLine("Esse cliente já existe!!!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("CPF inválido!!!");
+            }
+        }
+    }
+}
