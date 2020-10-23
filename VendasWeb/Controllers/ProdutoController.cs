f346a2550c2ae32a323bbdea1d24535c330e3001
Diogo Deconto <dideconto@gmail.com>
2020-10-23 16:44:48 -0300

    Cadastrar Produto

--- VendasWeb/Controllers/ProdutoController.cs
+++ VendasWeb/Controllers/ProdutoController.cs
@@ -1,16 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VendasWeb.Models;
 
 namespace VendasWeb.Controllers
 {
     public class ProdutoController : Controller
     {
+        private readonly Context _context;
+        public ProdutoController(Context context) => _context = context;
         public IActionResult Index()
         {
             return View();
         }
-        public string ActionDois()
+        public IActionResult Cadastrar() => View();
+        [HttpPost]
+        public IActionResult Cadastrar(string Nome, string Descricao, int Quantidade,
+           double Preco)
         {
-            return "Retorno de um texto";
+            Produto produto = new Produto
+            {
+                Nome = Nome,
+                Descricao = Descricao,
+                Quantidade = Quantidade,
+                Preco = Preco
+            };
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Produto");
         }
     }
 }
