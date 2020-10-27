11dd276cd053ffb3bb0e2b1113dc2e84b1b68755
Diogo Deconto <dideconto@gmail.com>
2020-10-26 22:14:18 -0300

    CRUD completo de Produto

--- VendasWeb/Controllers/ProdutoController.cs
+++ VendasWeb/Controllers/ProdutoController.cs
@@ -1,17 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VendasWeb.DAL;
 using VendasWeb.Models;
 
 namespace VendasWeb.Controllers
 {
     public class ProdutoController : Controller
     {
-        private readonly Context _context;
-        public ProdutoController(Context context) => _context = context;
+        private readonly ProdutoDAO _produtoDAO;
+        public ProdutoController(ProdutoDAO produtoDAO) => _produtoDAO = produtoDAO;
         public IActionResult Index()
         {
+            ViewBag.Produtos = _produtoDAO.Listar();
             return View();
         }
         public IActionResult Cadastrar() => View();
+
         [HttpPost]
         public IActionResult Cadastrar(string Nome, string Descricao, int Quantidade,
            double Preco)
@@ -23,8 +26,30 @@ namespace VendasWeb.Controllers
                 Quantidade = Quantidade,
                 Preco = Preco
             };
-            _context.Produtos.Add(produto);
-            _context.SaveChanges();
+            _produtoDAO.Cadastrar(produto);
+            return RedirectToAction("Index", "Produto");
+        }
+        public IActionResult Remover(int id)
+        {
+            _produtoDAO.Remover(id);
+            return RedirectToAction("Index", "Produto");
+        }
+        public IActionResult Alterar(int id)
+        {
+            ViewBag.Produto = _produtoDAO.BuscarPorId(id);
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Alterar(string Nome, string Descricao, int Quantidade,
+           double Preco, int Id, int HdnId)
+        {
+            Produto produto = _produtoDAO.BuscarPorId(Id);
+            produto.Nome = Nome;
+            produto.Descricao = Descricao;
+            produto.Quantidade = Quantidade;
+            produto.Preco = Preco;
+
+            _produtoDAO.Alterar(produto);
             return RedirectToAction("Index", "Produto");
         }
     }
