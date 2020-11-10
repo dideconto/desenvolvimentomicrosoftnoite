b9d4effdd9531db1962f07dbc176e881edda9bb8
Diogo Deconto <dideconto@gmail.com>
2020-11-09 22:03:38 -0300

    Upload de Imagem

--- VendasWeb/Migrations/20201109231337_AddImagemTableProduto.Designer.cs
+++ VendasWeb/Migrations/20201109231337_AddImagemTableProduto.Designer.cs
@@ -3,43 +3,23 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VendasWeb.Models;
 
 namespace VendasWeb.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20201109231337_AddImagemTableProduto")]
+    partial class AddImagemTableProduto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("ProductVersion", "3.1.9")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
-
-            modelBuilder.Entity("VendasWeb.Models.Categoria", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
-
-                    b.Property<DateTime>("CriadoEm")
-                        .HasColumnType("datetime2");
-
-                    b.Property<string>("Descricao")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("Nome")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("Categorias");
-                });
 
             modelBuilder.Entity("VendasWeb.Models.Produto", b =>
                 {
--- VendasWeb/Models/Categoria.cs
+++ VendasWeb/Models/Categoria.cs
@@ -1,10 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace VendasWeb.Models
 {
     [Table("Categorias")]
     public class Categoria : BaseModel
     {
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public string Nome { get; set; }
+
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
     }
 }
